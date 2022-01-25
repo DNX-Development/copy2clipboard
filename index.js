d93a895dd02d043b3ce9fdbd224f1cbb0aa3c585
Deeonix <svenpiedziak+github@gmail.com>
2022-01-25 18:39:08 +0100

    Add bootstrap

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const { readFile } = require('fs')
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 
 app.get('/', (request, response) => {
     readFile('./home.html', 'utf8', (err, data) => {
