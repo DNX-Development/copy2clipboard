d3caf823b53da750eb0f4185767245d92d3fb684
Deeonix <svenpiedziak+github@gmail.com>
2022-01-25 21:59:03 +0100

    Add function

--- index.js
+++ index.js
@@ -16,4 +16,18 @@ app.get('/', (request, response) => {
     })
 })
 
+app.get('/copy', (request, response) => {
+    if(request.query['text'] && request.query['text'] !== '') {
+        require("child_process").spawn("clip").stdin.end(request.query['text'])
+        readFile('./copied.html', 'utf8', (err, data) => {
+            if(err) {
+                response.status(500).send()
+            }
+            const new_data = data.toString('utf8').replace('%copytext%', request.query['text'])
+
+            response.send(new_data)
+        })
+    }
+})
+
 app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'))
